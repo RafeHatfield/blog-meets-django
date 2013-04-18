4a19a80e80f5fb36290008b4079ec026d7d440c7
RafeHatfield <rafe.hatfield@suite101.com>
2013-04-18 00:23:45 -0700

    updated migration for blogengine

--- blogengine/views.py
+++ blogengine/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from blogengine.models import Post
+
+def getRecentPosts(request):
+    # Get all blog posts
+    posts = Post.objects.all()
+
+    # Sort posts into chronological order
+    sorted_posts = posts.order_by('-pub_date')
+
+    # Display all the posts
+    return render_to_response('posts.html', { 'posts':sorted_posts})
