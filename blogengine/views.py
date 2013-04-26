18ac536a31713dcd238eeacd1cdd7dd83bacf209
RafeHatfield <rafe.hatfield@suite101.com>
2013-04-25 23:32:22 -0700

    added pager and empty page check

--- blogengine/views.py
+++ blogengine/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 from blogengine.models import Post
+
 
 def getPosts(request, selected_page=1):
     # Get all blog posts
@@ -8,10 +9,16 @@ def getPosts(request, selected_page=1):
 
     # Add pagination
     pages = Paginator(posts, 5)
-    returned_page = pages.page(selected_page)
+
+    # Get the specified page
+    try:
+        returned_page = pages.page(selected_page)
+    except EmptyPage:
+        returned_page = pages.page(pages.num_pages)
 
     # Display all the posts
-    return render_to_response('posts.html', {'posts':returned_page.object_list})
+    # return render_to_response('posts.html', { 'posts':returned_page.object_list})
+    return render_to_response('posts.html', { 'posts':returned_page.object_list, 'page':returned_page})
 
 def getPost(request, postSlug):
     # Get specified post
