7bd62847f2f5542d0921d19bf6371e7cf31b0768
RafeHatfield <rafe.hatfield@suite101.com>
2013-05-01 16:21:02 -0700

    updated paths to work across environments

--- blog_meets_django/settings.py
+++ blog_meets_django/settings.py
@@ -1,10 +1,15 @@
 # Django settings for blog_meets_django project.
+import os
+import sys
+
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
+    ('Rafe Hatfield', 'rafe.hatfield@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -61,7 +66,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(DIRNAME, '../static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,6 +77,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(DIRNAME, '../static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -111,7 +117,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/rafe/workspace/python/django/blog_meets_django/templates'
+    # '/Users/rafe/workspace/python/django/blog_meets_django/templates'
+    os.path.join(DIRNAME, '../templates'),
 )
 
 INSTALLED_APPS = (
